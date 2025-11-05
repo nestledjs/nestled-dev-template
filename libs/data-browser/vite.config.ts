b0be9401ef18e40fc71b6fc059cea03fe3f5077a
Justin Handley <justin@pirateandfox.com>
2025-11-05 16:20:07 -0400

    Update to modern transforms

--- libs/data-browser/vite.config.ts
+++ libs/data-browser/vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic',
+      jsxRuntime: 'automatic',
     }),
     dts({
       outDir: resolve(__dirname, '../../dist/libs/data-browser'),
