ec44f5a84ce6b48bd721f471315327ff30641986
Justin Handley <justin@pirateandfox.com>
2025-11-05 17:46:59 -0400

    Update build

--- libs/data-browser/vite.config.ts
+++ libs/data-browser/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       tsconfigPath: resolve(__dirname, './tsconfig.lib.json'),
     }),
   ],
+  mode: 'production',
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
